293e99c52cf30dd8d30e7befa92c1bf695cedb7c
jonrankin <jon.rankin0@gmail.com>
2024-03-05 13:04:35 -0500

    environment variable and proper formatting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { MongoClient } from 'mongodb';
 import { parseString, Builder } from "xml2js";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const uri = process.env.URI as string // Replace with your MongoDB connection string
+const dbName = process.env.DB as string  // Replace with your database name
+const collectionName = process.env.COLLECTION as string// Replace with your collection name
+
+if (!uri) { throw new Error("Environment variable URI is not set"); }
+if (!dbName) { throw new Error("Environment variable DB is not set"); }
+if (!collectionName) { throw new Error("Environment variable COLLECTION is not set"); }
 
 async function connectAndPrintDocuments() {
-    const uri = 'ConnectionString'; // Replace with your MongoDB connection string
-    const dbName = 'DB'; // Replace with your database name
-    const collectionName = 'collectionName'; // Replace with your collection name
+
 
     const collection_out = `${collectionName}_out`
     const client = await MongoClient.connect(uri);
@@ -16,18 +25,22 @@ async function connectAndPrintDocuments() {
         const documents = await collection.find().toArray();
 
 
+        // const newDoc = await parseString(documents[0].xml_value, { explicitArray: false, ignoreAttrs: true }, function(error, result) {
+        //     console.log(result.row)
+        // })
 
         documents.forEach(async (document) => {
             let data = {} as any
-            const newDoc = parseString(documents[0].xml_value, { explicitArray: false }, function(error, result) {
+            const newDoc = await parseString(document.xml_value, { explicitArray: false, ignoreAttrs: true }, function(error, result) {
                 data = result
             }) ;
-
-            await db.collection(collection_out).insertOne({
-                recid: documents[0].recid,
-                ...data.row
-            })
-            console.log(`Input ID: ${document.recid}`)
+            if(data) {
+                await db.collection(collection_out).insertOne({
+                    recid: document.recid,
+                    ...data.row
+                })
+            }
+            console.log('Document Inserted:', document.recid)
         });
 
     } catch (error) {
